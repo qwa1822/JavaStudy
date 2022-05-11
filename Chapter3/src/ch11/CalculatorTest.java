8ffbcbe924bf4df24e2abc3f171a89d22474c618
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-05-11 20:25:01 +0900

    인터페이스

---
+++ Chapter3/src/ch11/CalculatorTest.java
@@ -0,0 +1,21 @@
+package ch11;
+
+public class CalculatorTest {
+	public static void main(String[] args) {
+		
+		int num1=10;
+		int num2=2;
+		
+		Calc calc=new CompleteCalc();
+		//≈∏¿‘¿∫ calc   
+		System.out.println(calc.add(num1, num2));
+		System.out.println(calc.substract(num1, num2));
+		System.out.println(calc.times(num1, num2));
+		System.out.println(calc.divide(num1, num2));
+		
+		
+		
+		
+	}
+
+}
