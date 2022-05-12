5f35866662a0e827734583e90fee90a54d7275bd
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-05-12 12:33:08 +0900

    class

---
+++ Chatper4/src/ch03_String/TextBlockTest.java
@@ -0,0 +1,22 @@
+package ch03_String;
+
+public class TextBlockTest {
+
+	public static void main(String[] args) {
+
+		
+		
+		String textBlocks="""
+				æ»≥Á«œººø©,
+				π›∞©Ωø¥Ÿ,
+				how r u 
+				
+				""";
+		System.out.println(textBlocks);
+		
+		
+
+		
+	}
+
+}
