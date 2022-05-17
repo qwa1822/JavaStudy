a03c8bec251376624fc9f2a5a945572ab126fcaa
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-05-17 18:31:37 +0900

    합

---
+++ BackJun/src/반복문/BackJun_8393.java
@@ -0,0 +1,24 @@
+package ¹Ýº¹¹®;
+
+import java.util.Scanner;
+
+public class BackJun_8393 {
+
+	public static void main(String[] args) {
+
+		
+		Scanner sc=new Scanner(System.in);
+		
+		
+		int i=sc.nextInt();
+		
+		int sum=0;
+		for(int a=0; a<=i; a++) {
+			sum+=a;
+		}
+		
+		System.out.println(sum);
+		
+	}
+
+}
