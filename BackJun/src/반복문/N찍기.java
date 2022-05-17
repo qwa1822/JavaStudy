f9a498825d73e67684e7a19cc5385db261acb0fb
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-05-17 18:35:08 +0900

    N찍기

---
+++ BackJun/src/반복문/N찍기.java
@@ -0,0 +1,22 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class N찍기 {
+
+	public static void main(String[] args) {
+
+		
+		Scanner sc=new Scanner(System.in);
+		
+		
+		int i=sc.nextInt();
+		
+		
+		
+		for(int a=1; a<=i; a++) {
+			System.out.println(a);
+		}
+	}
+
+}
