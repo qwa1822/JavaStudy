6ff9e077008d7618dc78b91823037ed5cb651a8d
'qwa1822' <'asdzxc1822@naver.com'>
2022-06-02 00:04:16 +0900

    StringBuilder

---
+++ 자바복습/src/ch10/BirthdayTest.java
@@ -0,0 +1,18 @@
+package ch10;
+
+public class BirthdayTest {
+
+	public static void main(String[] args) {
+
+		BirthDay date=new BirthDay();
+		
+		date.SetYear(2022);
+		date.setMonth(12);
+		date.SetDay(30);
+			
+		
+		date.showDate();
+	
+	}
+
+}
